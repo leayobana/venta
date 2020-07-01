39b3a79efdcd189b1fb7e1312989d6e4c94c44f4
leayobana <leaquillx@gmail.com>
2020-07-01 03:01:36 -0600

    ventas

---
+++ vistas/modulos/clientes.php
@@ -0,0 +1,519 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar clientes
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar clientes</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      <div class="box-header with-border">
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCliente">
+          
+          Agregar cliente
+
+        </button>
+
+      </div>
+
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Nombre</th>
+           <th>Documento ID</th>
+           <th>Email</th>
+           <th>Teléfono</th>
+           <th>Dirección</th>
+           <th>Fecha nacimiento</th> 
+           <th>Total compras</th>
+           <th>Última compra</th>
+           <th>Ingreso al sistema</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+
+          $item = null;
+          $valor = null;
+
+          $clientes = ControladorClientes::ctrMostrarClientes($item, $valor);
+
+          foreach ($clientes as $key => $value) {
+            
+
+            echo '<tr>
+
+                    <td>'.($key+1).'</td>
+
+                    <td>'.$value["nombre"].'</td>
+
+                    <td>'.$value["documento"].'</td>
+
+                    <td>'.$value["email"].'</td>
+
+                    <td>'.$value["telefono"].'</td>
+
+                    <td>'.$value["direccion"].'</td>
+
+                    <td>'.$value["fecha_nacimiento"].'</td>             
+
+                    <td>'.$value["compras"].'</td>
+
+                    <td>'.$value["ultima_compra"].'</td>
+
+                    <td>'.$value["fecha"].'</td>
+
+                    <td>
+
+                      <div class="btn-group">
+                          
+                        <button class="btn btn-warning btnEditarCliente" data-toggle="modal" data-target="#modalEditarCliente" idCliente="'.$value["id"].'"><i class="fa fa-pencil"></i></button>';
+
+                      if($_SESSION["perfil"] == "Administrador"){
+
+                          echo '<button class="btn btn-danger btnEliminarCliente" idCliente="'.$value["id"].'"><i class="fa fa-times"></i></button>';
+
+                      }
+
+                      echo '</div>  
+
+                    </td>
+
+                  </tr>';
+          
+            }
+
+        ?>
+   
+        </tbody>
+
+       </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+<!--=====================================
+MODAL AGREGAR CLIENTE
+======================================-->
+
+<div id="modalAgregarCliente" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar cliente</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <form method="post">
+    <input type="text" class="ruc" id="ruc" name="ruc">
+
+
+
+
+   
+
+    <!--button id="botoncito" class="botoncito"><i class="fa fa-search"></i> Buscar</button-->
+    <img src="ajax.gif" class="ajaxgif hide">
+  </form>
+   <button type="button" id="X" class="botoncito" onclick="consulta_ruc()" ><i class="fa fa-search"></i> Consulta RUC</button>
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input id="rucNombre" type="text" class="form-control input-lg" name="nuevoCliente" placeholder="Ingresar nombre" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL DOCUMENTO ID -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input id="rucNumero" type="number" min="0" class="form-control input-lg" name="nuevoDocumentoId" placeholder="Ingresar documento" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL EMAIL -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span> 
+
+                <input id="rucNombre" type="email" class="form-control input-lg" name="nuevoEmail" placeholder="Ingresar email" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL TELÉFONO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoTelefono" placeholder="Ingresar teléfono" data-inputmask="'mask':'(999) 999-9999'" data-mask required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA DIRECCIÓN -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-map-marker"></i></span> 
+
+                <input id="rucDireccion" type="text" class="form-control input-lg" name="nuevaDireccion" placeholder="Ingresar dirección" required>
+
+              </div>
+
+            </div>
+
+             <!-- ENTRADA PARA LA FECHA DE NACIMIENTO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevaFechaNacimiento" placeholder="Ingresar fecha nacimiento" data-inputmask="'alias': 'yyyy/mm/dd'" data-mask required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cliente</button>
+
+        </div>
+
+      </form>
+
+      <?php
+
+        $crearCliente = new ControladorClientes();
+        $crearCliente -> ctrCrearCliente();
+
+      ?>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!--=====================================
+MODAL EDITAR CLIENTE
+======================================-->
+
+<div id="modalEditarCliente" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar cliente</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarCliente" id="editarCliente" required>
+                <input type="hidden" id="idCliente" name="idCliente">
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL DOCUMENTO ID -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input type="number" min="0" class="form-control input-lg" name="editarDocumentoId" id="editarDocumentoId" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL EMAIL -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span> 
+
+                <input type="email" class="form-control input-lg" name="editarEmail" id="editarEmail" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL TELÉFONO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarTelefono" id="editarTelefono" data-inputmask="'mask':'(999) 999-9999'" data-mask required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA DIRECCIÓN -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-map-marker"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarDireccion" id="editarDireccion"  required>
+
+              </div>
+
+            </div>
+
+             <!-- ENTRADA PARA LA FECHA DE NACIMIENTO -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="editarFechaNacimiento" id="editarFechaNacimiento"  data-inputmask="'alias': 'yyyy/mm/dd'" data-mask required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+
+        </div>
+
+      </form>
+
+      <?php
+
+        $editarCliente = new ControladorClientes();
+        $editarCliente -> ctrEditarCliente();
+
+      ?>
+
+    
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php
+
+  $eliminarCliente = new ControladorClientes();
+  $eliminarCliente -> ctrEliminarCliente();
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Consulta SUNAT</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<script src="js/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+<script>
+
+function consulta_ruc()
+{
+
+var ruc = $('#ruc').val();
+ $.ajax({
+                url: 'https://api.migoperu.pe/api/v1/ruc?token=bb59193c-4251-4121-9dd1-f4a3dadfb4b9-4345647c-5ff7-4b2c-9c53-75b988f69b5c  &ruc='+ruc,
+                dataType: 'json',
+                type: 'post',
+                contentType: "application/json; charset=utf-8",
+                success: function( data, textStatus, jQxhr ){
+                  $('#rucNombre').val(data.nombre_o_razon_social);
+                  $('#rucDireccion').val(data.direccion);
+                  $('#rucNumero').val(data.ruc);
+                    console.log("BIEN FUNCIONA"+JSON.stringify(data));
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                  console.log( "--------------------------------");
+                    console.log( errorThrown );
+                    console.log( "--------------------------------");
+                }
+            }); 
+ 
+
+}
+
+
+
+$(function(){
+  $('#botoncito').on('click', function(){
+    var ruc = $('#ruc').val();
+    var url = 'consulta_sunat.php';
+    $('.ajaxgif').removeClass('hide');
+    $.ajax({
+    type:'POST',
+    url:url,
+    data:'ruc='+ruc,
+    success: function(datos_dni){
+      $('.ajaxgif').addClass('hide');
+      var datos = eval(datos_dni);
+        var nada ='nada';
+        if(datos[0]==nada){
+          alert('DNI o RUC no válido o no registrado');
+        }else{
+          $('#numero_ruc').text(datos[0]);
+          $('#nuevoCliente').val(datos[1]);
+          $('#fecha_actividad').text(datos[2]);
+          $('#condicion').text(datos[3]);
+          $('#tipo').text(datos[4]);
+          $('#estado').text(datos[5]);
+          $('#fecha_inscripcion').text(datos[6]);
+          $('#domicilio').text(datos[7]);
+          $('#emision').text(datos[8]);
+        }   
+    }
+  });
+  return false;
+  });
+});
+</script>
+</body>
+</html>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2018 <a href="" target="_blank">Universidad Peruana Union</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
